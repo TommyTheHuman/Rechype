2b48a096c76d2eae34fdf62a87d311ef2168daaf
TommyTheHuman <tommyc98@yahoo.it>
2021-04-21 22:56:12 +0200

    added correct path for landing.fxml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("landing.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("it/unipi/dii/inginf/lsmdb/rechype/gui/landing.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
