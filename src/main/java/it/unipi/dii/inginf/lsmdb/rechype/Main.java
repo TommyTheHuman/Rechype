8fda616db013d0839342baab79449a5e91c23e07
TommyTheHuman <tommyc98@yahoo.it>
2021-04-30 15:26:14 +0200

    Added: change scene on registration

--- src/main/java/it/unipi/dii/inginf/lsmdb/rechype/Main.java
+++ src/main/java/it/unipi/dii/inginf/lsmdb/rechype/Main.java
@@ -1,3 +1,5 @@
+package it.unipi.dii.inginf.lsmdb.rechype;
+
 import it.unipi.dii.inginf.lsmdb.rechype.user.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -5,21 +7,35 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    private static Scene mainScene;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("landing.fxml"));
+        mainScene = new Scene(loadFXML("landing"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(mainScene);
         primaryStage.show();
     }
 
     private static final UserServiceFactory factory=UserServiceFactory.create();
 
     public static void main(String[] args){
+
         launch(args);
         UserService userService=factory.getService();
 
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/" + fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void changeScene(String fxml) throws Exception{
+        mainScene.setRoot(loadFXML(fxml));
     }
 }
