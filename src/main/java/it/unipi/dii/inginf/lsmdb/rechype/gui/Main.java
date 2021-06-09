f0271d7ffd6f08888edb17d8444cc59f43db19d3
NikoSalamini <nikosalamini@gmail.com>
2021-06-09 10:26:36 +0200

    populate remove, final commit

--- src/main/java/it/unipi/dii/inginf/lsmdb/rechype/gui/Main.java
+++ src/main/java/it/unipi/dii/inginf/lsmdb/rechype/gui/Main.java
@@ -1,9 +1,5 @@
 package it.unipi.dii.inginf.lsmdb.rechype.gui;
 
-import com.mongodb.MongoException;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import it.unipi.dii.inginf.lsmdb.rechype.population.Populate;
 import it.unipi.dii.inginf.lsmdb.rechype.util.JSONAdder;
 import it.unipi.dii.inginf.lsmdb.rechype.persistence.HaloDBDriver;
 import it.unipi.dii.inginf.lsmdb.rechype.persistence.MongoDriver;
@@ -14,29 +10,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
-import org.bson.Document;
 import org.json.JSONObject;
-import org.neo4j.driver.Session;
-import org.neo4j.driver.TransactionWork;
-import org.neo4j.driver.exceptions.Neo4jException;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import static org.neo4j.driver.Values.parameters;
 
 public class Main extends Application {
     private static Scene mainScene;
 
     @Override
     public void start(Stage primaryStage) {
-        //populateNeo4j();
-        //Populate pop=new Populate();
         mainScene = new Scene(loadFXML("Landing", new JSONObject()), 1000, 700);
         primaryStage.setTitle("Rechype");
         primaryStage.setScene(mainScene);
@@ -52,85 +34,6 @@ public class Main extends Application {
 
     public static void main(String[] args){
         launch(args);
-    }
-
-    //populating neo4j
-    private static void populateNeo4j() {
-        System.out.println("ehi");
-        MongoCollection<Document> recipesColl = MongoDriver.getObject().getCollection(MongoDriver.Collections.RECIPES);
-        MongoCollection<Document> drinksColl = MongoDriver.getObject().getCollection(MongoDriver.Collections.DRINKS);
-        MongoCollection<Document> ingredientsColl = MongoDriver.getObject().getCollection(MongoDriver.Collections.INGREDIENTS);
-
-        //INGREDIENTS
-        try (MongoCursor<Document> cursor = ingredientsColl.find().iterator()) {
-            while (cursor.hasNext()) {
-                Document doc = cursor.next();
-
-                //inserting ingredients on neo4j
-                try (Session session = Neo4jDriver.getObject().getDriver().session()) { //try to add
-                    session.writeTransaction((TransactionWork<Void>) tx -> {
-                        if(doc.get("image")!=null) {
-                            tx.run("CREATE (i:Ingredient { id:$id, imageUrl: $imageUrl})",
-                            parameters("id", doc.getString("_id"),
-                                    "imageUrl", doc.getString("image")));
-                        }
-                        else
-                            tx.run("CREATE (i:Ingredient { id:$id })",
-                                    parameters("id", doc.getString("_id")));
-                        return null;
-                    });
-                }catch(Neo4jException ne){
-                    ne.printStackTrace();
-                }
-            }
-        } catch (MongoException me) {
-            me.printStackTrace();
-        }
-
-        //RECIPES
-        try (MongoCursor<Document> cursor = recipesColl.find().iterator()) {
-            while (cursor.hasNext()) {
-                Document doc = cursor.next();
-
-                //inserting recipes on neo4j
-                try (Session session = Neo4jDriver.getObject().getDriver().session()) { //try to add
-                    session.writeTransaction((TransactionWork<Void>) tx -> {
-
-                        tx.run("CREATE (r:Recipe { id:$id, name: $name, author: $author, pricePerServing: $pricePerServing, imageUrl: $imageUrl," +
-                                        "vegetarian: $vegetarian, vegan: $vegan, dairyFree: $dairyFree, glutenFree: $glutenFree} )",
-                                parameters("id", doc.getObjectId("_id").toString(), "name", doc.getString("name"), "author", doc.getString("author"), "pricePerServing", doc.get("pricePerServing"),
-                                        "imageUrl", doc.getString("image"), "vegetarian", doc.getBoolean("vegetarian"), "vegan", doc.getBoolean("vegan"), "dairyFree", doc.getBoolean("dairyFree"),
-                                        "glutenFree", doc.getBoolean("glutenFree")));
-                        return null;
-                    });
-                }catch(Neo4jException ne){
-                    ne.printStackTrace();
-                }
-            }
-        } catch (MongoException me) {
-            me.printStackTrace();
-        }
-
-        //DRINKS
-        try (MongoCursor<Document> cursor = drinksColl.find().iterator()) {
-            while (cursor.hasNext()) {
-                Document doc = cursor.next();
-                try (Session session = Neo4jDriver.getObject().getDriver().session()) { //try to add
-                    session.writeTransaction((TransactionWork<Void>) tx -> {
-                        tx.run("CREATE (d:Drink { id:$id, name: $name, author: $author, imageUrl: $imageUrl, " +
-                                        "tag: $tag})",
-                                parameters("id", doc.getObjectId("_id").toString(), "name", doc.getString("name"), "author", doc.getString("author"),
-                                        "imageUrl", doc.getString("image"), "tag", doc.getString("tag")));
-                        return null;
-                    });
-                }catch(Neo4jException ne){
-                    ne.printStackTrace();
-                }
-            }
-        }catch (MongoException me) {
-            me.printStackTrace();
-        }
-        System.out.println("ok");
     }
 
     private static Parent loadFXML(String fxml, JSONObject par){
